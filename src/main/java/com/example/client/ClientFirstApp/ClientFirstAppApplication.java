5a9d218c1e7a7a3bd6dda5080fe84c064da0b022
Moiz Amjherawala <moiz.amjherawala@yash.com>
2021-05-31 15:52:20 +0530

    initial commit

---
+++ src/main/java/com/example/client/ClientFirstApp/ClientFirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.client.ClientFirstApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientFirstAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/client/ClientFirstApp/ClientFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.client.ClientFirstApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
